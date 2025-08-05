0810fd85572e30c5570d8c4b96fa50ce18ef421a
VINICIUS JULIO DE OLIVEIRA <25132898@CFP127ALUNO.LOCAL>
2025-08-05 16:57:14 -0300

    Entrada de nomes em andamento

---
+++ modulo/names.js
@@ -0,0 +1,18 @@
+/********************************************************
+ * Objetivo: Criar função para receber o nome do cliente e do produto
+ * Autor: Vinicius
+ * Data: 05/08/2025
+ * Versão 1.0 
+***********************************************************/
+
+//Função que recebe o Nome do cliente e do produto 
+function nomes(cliente, produto) {
+    let nomeCliente = cliente 
+    let nomeProduto = produto
+
+    if (nomeClienteliente == undefined || nomeProduto == undefined ) {
+        return false
+    }else{
+        return nomeCliente, nomeProduto
+    }
+}
